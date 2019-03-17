ba718955c1b139000f962add37e36f29cc072802
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 01:37:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here:
+
+function driversWithRevenueOver (drivers, revenue){
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue
+  })
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) { 
+    return driver.name
+  })
+}
+
+function exactMatch(drivers, attr) {
+  let k = Object.keys(attr)[0];
+  let v = attr[k];
+  return drivers.filter(
+    
+    driver => driver[k] === v)
+}
+
+
+function exactMatchToList(drivers, attr) {
+  return exactMatch(drivers, attr).map(function (driver) {
+    return driver.name
+  })
+}
